fea441af40d53120a0b44577fddd49cbff9917ad
Ny ANDRIANTSOA <nyandriantsoa25@gmail.com>
2020-10-28 18:06:56 -0400

    api gateway initialized

---
+++ src/main/java/com/imamiako/storeapigateway/domain/User.java
@@ -0,0 +1,12 @@
+package com.imamiako.storeapigateway.domain;
+
+public class User {
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private String email;
+    private String facebook;
+
+}
---
+++ src/test/java/com/imamiako/storeapigateway/StoreApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imamiako.storeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
